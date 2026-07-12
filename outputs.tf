ea04b8ebfe33336dbc55ab96a3bc4d20cf742c19
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:20:27 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,21 +1,17 @@
-output "site_recovery_protection_containers" {
-  description = "All site_recovery_protection_container resources"
-  value       = azurerm_site_recovery_protection_container.site_recovery_protection_containers
-}
 output "site_recovery_protection_containers_name" {
-  description = "List of name values across all site_recovery_protection_containers"
-  value       = [for k, v in azurerm_site_recovery_protection_container.site_recovery_protection_containers : v.name]
+  description = "Map of name values across all site_recovery_protection_containers, keyed the same as var.site_recovery_protection_containers"
+  value       = { for k, v in azurerm_site_recovery_protection_container.site_recovery_protection_containers : k => v.name }
 }
 output "site_recovery_protection_containers_recovery_fabric_name" {
-  description = "List of recovery_fabric_name values across all site_recovery_protection_containers"
-  value       = [for k, v in azurerm_site_recovery_protection_container.site_recovery_protection_containers : v.recovery_fabric_name]
+  description = "Map of recovery_fabric_name values across all site_recovery_protection_containers, keyed the same as var.site_recovery_protection_containers"
+  value       = { for k, v in azurerm_site_recovery_protection_container.site_recovery_protection_containers : k => v.recovery_fabric_name }
 }
 output "site_recovery_protection_containers_recovery_vault_name" {
-  description = "List of recovery_vault_name values across all site_recovery_protection_containers"
-  value       = [for k, v in azurerm_site_recovery_protection_container.site_recovery_protection_containers : v.recovery_vault_name]
+  description = "Map of recovery_vault_name values across all site_recovery_protection_containers, keyed the same as var.site_recovery_protection_containers"
+  value       = { for k, v in azurerm_site_recovery_protection_container.site_recovery_protection_containers : k => v.recovery_vault_name }
 }
 output "site_recovery_protection_containers_resource_group_name" {
-  description = "List of resource_group_name values across all site_recovery_protection_containers"
-  value       = [for k, v in azurerm_site_recovery_protection_container.site_recovery_protection_containers : v.resource_group_name]
+  description = "Map of resource_group_name values across all site_recovery_protection_containers, keyed the same as var.site_recovery_protection_containers"
+  value       = { for k, v in azurerm_site_recovery_protection_container.site_recovery_protection_containers : k => v.resource_group_name }
 }
 
